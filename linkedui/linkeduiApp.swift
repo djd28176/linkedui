fff87153057c0c6a9a33dd46e03ca0dd002d4d9a
jindongdu <djd28176@hotmail.com>
2023-12-07 11:11:55 -0500

    Initial Commit

---
+++ linkedui/linkeduiApp.swift
@@ -0,0 +1,17 @@
+//
+//  linkeduiApp.swift
+//  linkedui
+//
+//  Created by Jindong Du on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct linkeduiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
